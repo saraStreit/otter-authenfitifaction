c7b7267ede6f1d98303990ec8dddd2cddda62797
deadepona <sara.streit@ag.ch>
2023-04-03 20:51:56 +0200

    add previous controlers

---
+++ otterApi/otterApi/Controllers/jwt-token.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text.Json;
+
+namespace otterApi.Controllers
+{
+    public class jwt_token
+    {
+        public string GenerateJwtKey()
+        {
+            var key = new byte[32];
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                generator.GetBytes(key);
+            }
+
+            var base64Key = Convert.ToBase64String(key);
+
+            var appSettings = new
+            {
+                Jwt = new
+                {
+                    Key = base64Key
+                }
+            };
+            var json = JsonSerializer.Serialize(appSettings, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            return json;
+        }
+    }
+}
