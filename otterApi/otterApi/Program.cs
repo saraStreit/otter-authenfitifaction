1629e3ec0e181bcc0da76f355bb2d133cafc85a0
deadepona <sara.streit@ag.ch>
2023-04-03 20:49:10 +0200

    recover standard settings

--- otterApi/otterApi/Program.cs
+++ otterApi/otterApi/Program.cs
@@ -1,12 +1,8 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+// Add services to the container.
 
-builder.Services.AddAuthorization()
-builder.Services.AddAuthentication().AddCookie();
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,15 +18,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
-
 app.MapControllers();
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/login", (HttpContext context) =>
-{
-    context.SignInAsync(new ClaimsPrincipal() )
-})
 
 app.Run();
