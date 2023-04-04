4285b9f4dd045a4b2397aa53b6d589421fe7de7b
Sara <Sara.streit@ag.ch>
2023-04-04 16:53:11 +0200

    add otter controller and remove unused projects

---
+++ otterApi/otterApi/Controllers/OtterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace otterApi.Controllers
+{
+        [ApiController]
+        [Route("[controller]")]
+        public class SessionController : ControllerBase
+        {
+            [HttpPost("set")]
+            public IActionResult SetSessionValue(string key, string value)
+            {
+                HttpContext.Session.SetString(key, value);
+                return Ok();
+            }
+        }
+}
